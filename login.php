0a81f4448fb04f91664175a6e46ea10dc8767f40
dimitrispap1072583 <128745981+dimitrispap1072583@users.noreply.github.com>
2023-09-14 02:11:07 +0300

    Add files via upload

--- login.php
+++ login.php
@@ -1,10 +1,80 @@
-sqluser="SELECT * FROM users WHERE user_name='$uname' AND password='$pass AND admin=0;'";
+<?php
+session_start();
+include "database_connection.php";
+
+$login_error = ''; //Arxikopoihsh mhnymatos gia login error
+
+if(isset($_POST['email']) && isset($_POST['password']))
+{
+    function validate($data) //Synarthsh poy katharizei ta dwsmena dedomena
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $email = validate($_POST['email']);
+    $pass = validate($_POST['password']);
+
+    if(empty($email)) //An den yparxei username tote emfanise error
+    {
+        header("Location: index.php?error=User Name is required");
+        exit();
+    }
+    else if(empty($pass)) //An den yparxei password submitted tote emfanise error
+    {
+        header("Location: index.php?error=Password is required");
+        exit();
+    }
+
+    $sql = "SELECT * FROM user WHERE email='$email' AND password='$pass'"; 
+   
+
+    $result = mysqli_query($conn, $sql);
+    
+
+    if(mysqli_num_rows($result) === 1) {
+    while ($row = mysqli_fetch_assoc($result))                            {
+    if($row['email'] === $email && $row['password'] === $pass) {
+       
+         if($row['admin']==0){
+            echo "You are now logged in user.";
+            $_SESSION['username'] = $row['username'];
+            $_SESSION['email'] = $row['email'];
+            $_SESSION['user_id'] = $row['user_id'];
+            header("Location: testmap.php");
+            exit();           
+}
+         else{
+            echo "You are now logged in admin.";
+            $_SESSION['username'] = $row['username'];
+            $_SESSION['email'] = $row['email'];
+            $_SESSION['user_id'] = $row['user_id'];
+            header("Location: admin_page.php");
+            exit();           
+}
 
 
-sqladmin="SELECT * FROM users WHERE user_name='$uname' AND password='$pass AND admin=1;'";
 
-
-resultuser=mysqli_query($conn, $sqluser);
-
-resultadmin=mysqli_query($conn, $sqladmin);
-
+        }
+        else
+        {
+            header("Location: index.php?error=Incorrect User Name or Password. Please try again or sign up1");
+            exit();
+        }
+    }
+    }
+    else
+    {
+        header("Location: index.php?error=Incorrect User Name or Password. Please try again or sign up2");
+        exit();
+    }
+    
+}
+else
+{
+    header("Location: index.php");
+    exit();
+}
+?>
