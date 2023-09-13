9088bc855cb9321d9c7b18bf7ab0b04689c0f782
dimitrispap1072583 <128745981+dimitrispap1072583@users.noreply.github.com>
2023-09-13 18:37:57 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,74 +1,10 @@
-<?php 
-session_start(); 
+sqluser="SELECT * FROM users WHERE user_name='$uname' AND password='$pass AND admin=0;'";
 
 
-$connect = mysqli_connect("localhost:3307", "root", "", "web_project");
+sqladmin="SELECT * FROM users WHERE user_name='$uname' AND password='$pass AND admin=1;'";
 
-if (isset($_POST['email']) && isset($_POST['password'])) {
 
-	function validate($data){
-       $data = trim($data);
-	   $data = stripslashes($data);
-	   $data = htmlspecialchars($data);
-	   return $data;
-	}
+resultuser=mysqli_query($conn, $sqluser);
 
-	$email = validate($_POST['email']);
-	$pass = validate($_POST['password']);
+resultadmin=mysqli_query($conn, $sqladmin);
 
-	if (empty($email)) {
-		header("Location: index.php?error=Απαιτείται όνομα χρήστη");
-	    exit();
-	}else if(empty($pass)){
-        header("Location: index.php?error=Απαιτείται κωδικός");
-	    exit();
-	}else                                                                                         {
-		// hashing the password
-        $pass = md5($pass);
-
-        
-		$sql = "SELECT * FROM user WHERE email='$email' AND password='$pass'";
-
-		$result = mysqli_query($connect, $sql);
-
-		if (mysqli_num_rows($result) === 1) {
-		                                                                            {
-			$row = mysqli_fetch_assoc($result);
-												    
-            if ($row['email'] === $email && $row['password'] === $pass )  {
-            	if ($row['admin'] === 1){
-		$_SESSION['username'] = $row['username'];
-            	$_SESSION['user_id'] = $row['user_id'];
-		$_SESSION['email'] = $row['email'];
-		$_SESSION['registration_date'] = $row['registration_date'];
-		$_SESSION['admin'] = $row['admin'];
-			header("Location: admin_page.php");
-			exit();}
-			
-		                  else{
-		 $_SESSION['username'] = $row['username'];
-            	$_SESSION['user_id'] = $row['user_id'];
-		$_SESSION['email'] = $row['email'];
-		$_SESSION['registration_date'] = $row['registration_date'];
-		$_SESSION['admin'] = $row['admin'];
-            	                         header("Location: testmap.php");
-		                         exit();
-		                      }
-            	
-
-	    }
-		
-                                                                             }
-	else{
-		header("Location: index.php?error=Λάθος όνομα χρήστη ή κωδικός");
-		exit();
-	    }
-		                                                                       }
-		
-	                                                                                   }
-	
-}
-else{
-	header("Location: index.php");
-	exit();
-}
